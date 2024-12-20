1cdea26af684052e2c09fe415241cf6c6568cf5b
Dean Lane <dglaneto@gmail.com>
2024-12-20 18:09:07 -0500

    added random spawn time, speed & rotation

--- scripts/game.gd
+++ scripts/game.gd
@@ -3,20 +3,17 @@ extends Node2D
 @export var meteorite_rock_scene: PackedScene
 
 @onready var timer: Timer = $Timer
-
 # Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	spawn_rock()
-	print(meteorite_rock_scene)
+var spawn_rate_min:float = 2
+var spawn_rate_max:float = 4
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _ready() -> void:
+	# when the game first loads it will spawn a meteorite
+	spawn_rock()
 
 func spawn_rock() -> void:
-		var new_rock: meteorite = meteorite_rock_scene.instantiate()
-		var xpos: float = randf_range(70, 1140)
-		
+		var new_rock: Meteorite = meteorite_rock_scene.instantiate()
+		var xpos: float = randf_range(90, 1050)
 		# Setting random spawn location
 		new_rock.position = Vector2(xpos,-50)
 		
@@ -32,11 +29,13 @@ func game_over() -> void:
 	timer.stop()
 	print("Timer has been stopped")
 	
-
+ # timer signal to trigger when timed out
 func _on_timer_timeout() -> void:
+	timer.wait_time = randf_range(spawn_rate_min,spawn_rate_max)
+	print(timer.wait_time)
 	spawn_rock()
 
-
+# signal for dectecting if the Area2D on the cargo ship has had an object enter it.
 func _on_cargo_ship_area_entered(area: Area2D) -> void:
 	print("something hit the ship")
 	area.queue_free()
--- scripts/meteorite_rock.gd
+++ scripts/meteorite_rock.gd
@@ -1,8 +1,11 @@
 extends Area2D
 
-class_name meteorite
+class_name Meteorite
+@onready var sprite_2d: Sprite2D = $Sprite2D
 
-var speed = 100
+var speed:float = randf_range(50,200)
+var rotate_speed = 35
+
 signal meteorite_off_screen
 
 # Called when the node enters the scene tree for the first time.
@@ -12,7 +15,8 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	position.y += speed * delta
+	sprite_2d.rotate(rotate_speed * delta * (PI / 180))
 	if position.y > get_viewport_rect().size.y:
-		queue_free()
 		meteorite_off_screen.emit()
+		queue_free()
 		
