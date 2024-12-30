503ca3c9adf753854ab45fdc73b85df2e496ba95
Dean Lane <dglaneto@gmail.com>
2024-12-30 12:11:10 -0500

    function for game over delete rocks and scoring

--- scripts/game.gd
+++ scripts/game.gd
@@ -3,16 +3,26 @@ extends Node2D
 @export var meteorite_rock_scene: PackedScene
 
 @onready var timer: Timer = $Timer
+@onready var score: Label = $Score
+
 # Called when the node enters the scene tree for the first time.
 var spawn_rate_min:float = 2
 var spawn_rate_max:float = 4
+var player_score: int = 0
 
 func _ready() -> void:
+	print("Game Started")
 	# when the game first loads it will spawn a meteorite
 	spawn_rock()
+	
 
+# spawn meteorite at romdom locations off screen
 func spawn_rock() -> void:
 		var new_rock: Meteorite = meteorite_rock_scene.instantiate()
+		
+		# Setting meta data on the newly spawned meteorites so we can target and delete them later when game over
+		new_rock.set_meta("meteorite", true) # NEEDS TO BE REVIEWED
+		
 		var xpos: float = randf_range(90, 1050)
 		# Setting random spawn location
 		new_rock.position = Vector2(xpos,-50)
@@ -25,9 +35,25 @@ func spawn_rock() -> void:
 
 # function to handle game over
 func game_over() -> void:
-	print("triggered")
+	print("GAME OVER")
+	# Stop the timer
 	timer.stop()
-	print("Timer has been stopped")
+	# end all processes
+	
+	# delete alll remaining meteorites on the screen
+	for child in get_children():
+		
+		# looks trough all objects in the tree that is an Area2D and has the mete data key "meteorite" then remove them
+		if child is Area2D and child.has_meta("meteorite"):
+			child.queue_free()
+		# deletes 
+		elif "Meteorite-rock" in str(child):
+			child.queue_free()
+		
+		# stops all the proceses on all children 	
+		child.set_process(false)
+	
+	
 	
  # timer signal to trigger when timed out
 func _on_timer_timeout() -> void:
@@ -37,6 +63,8 @@ func _on_timer_timeout() -> void:
 
 # signal for dectecting if the Area2D on the cargo ship has had an object enter it.
 func _on_cargo_ship_area_entered(area: Area2D) -> void:
-	print("something hit the ship")
-	area.queue_free()
+	print("Add value")
+	player_score += 1000 # increment the players score by $1000
+	score.text = "Score: $" + str(player_score) # updates the score label
+	area.queue_free() # deletes the meteorite once it enters the ship
 	
