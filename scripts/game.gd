6bcf445d38658165de2915954c7d342a77717175
Dean Lane <dglaneto@gmail.com>
2025-01-03 23:45:56 -0500

    Sprite animations, random type meteorites & true scores

--- scripts/game.gd
+++ scripts/game.gd
@@ -11,18 +11,23 @@ var spawn_rate_max:float = 4
 var player_score: int = 0
 var player_lives: Array = [$Lives/Life]
 
+
 func _ready() -> void:
 	print("Game Started")
 	# when the game first loads it will spawn a meteorite
 	spawn_rock()
 	
-
 # spawn meteorite at romdom locations off screen
-func spawn_rock() -> void:
+func spawn_rock():
 		var new_rock: Meteorite = meteorite_rock_scene.instantiate()
+		# which rock is spawning?
+		print("rock spawned ", new_rock.spawned_rock)
+		#var rock_spawned = new_rock.
+		# What is the value?
+		# get the vaule to the score section
 		
-		# Setting meta data on the newly spawned meteorites so we can target and delete them later when game over
-		new_rock.set_meta("meteorite", true) # NEEDS TO BE REVIEWED
+		## Setting meta data on the newly spawned meteorites so we can target and delete them later when game over
+		#new_rock.set_meta("meteorite", true) # NEEDS TO BE REVIEWED
 		
 		var xpos: float = randf_range(90, 1050)
 		# Setting random spawn location
@@ -30,13 +35,23 @@ func spawn_rock() -> void:
 		
 		# function to fire when signal is emitted from the new_rock
 		new_rock.meteorite_off_screen.connect(game_over)
-		new_rock.meteorite_off_screen.connect(fail)
+		#new_rock.meteorite_off_screen.connect(fail)
 		
 		# adding the new_rock to the main game scene
 		add_child(new_rock)
-
-func fail() -> void:
-	player_lives.pop_back()
+		
+		print(new_rock.spawned_rock.keys()[0])
+		print(new_rock.spawned_rock.values()[0])
+		
+		# Setting meta data on the newly spawned meteorites so we can target and delete them later when game over
+		var rock_name = new_rock.spawned_rock.keys()[0]
+		var rock_value = new_rock.spawned_rock[rock_name]
+		new_rock.set_meta("meteorite", true) # Sets the meta data to meteorite which is true
+		new_rock.set_meta("type", rock_name) # sets the meta data of type and the name of the meteorite
+		new_rock.set_meta("value", rock_value) # sets the meta data of value and defines the amount for that rock
+		
+#func fail() -> void:
+	#player_lives.pop_back()
 
 # function to handle game over
 func game_over() -> void:
@@ -48,7 +63,7 @@ func game_over() -> void:
 	# delete alll remaining meteorites on the screen
 	for child in get_children():
 		
-		# looks trough all objects in the tree that is an Area2D and has the mete data key "meteorite" then remove them
+		# looks through all objects in the tree that is an Area2D and has the meta data key "meteorite" then remove them
 		if child is Area2D and child.has_meta("meteorite"):
 			child.queue_free()
 		# deletes 
@@ -63,13 +78,18 @@ func game_over() -> void:
  # timer signal to trigger when timed out
 func _on_timer_timeout() -> void:
 	timer.wait_time = randf_range(spawn_rate_min,spawn_rate_max)
-	print(timer.wait_time)
 	spawn_rock()
 
 # signal for dectecting if the Area2D on the cargo ship has had an object enter it.
 func _on_cargo_ship_area_entered(area: Area2D) -> void:
-	print("Add value")
-	player_score += 1000 # increment the players score by $1000
+	#var meta_data = area.get_meta_list()
+	#print()
+	var meteorite_value = null
+	if area.has_meta("value"):
+		meteorite_value = area.get_meta("value")
+		print("meteorite value: ", meteorite_value)
+
+	player_score += meteorite_value
 	score.text = "Score: $" + str(player_score) # updates the score label
 	area.queue_free() # deletes the meteorite once it enters the ship
 	
--- scripts/meteorite_rock.gd
+++ scripts/meteorite_rock.gd
@@ -4,14 +4,35 @@ class_name Meteorite
 @onready var sprite_2d: Sprite2D = $Sprite2D
 
 var speed:float = randf_range(50,200)
-var rotate_speed = 35
+var rotate_speed:int = 35
+var meteorite_arr = ["iron", "gold", "emerald", "platinum"]
+var meteorite_dict = {"iron":100, "gold":2000, "emerald":3000, "platinum":4000}
+var spawned_rock = {}
+
+# Define the number of columns and rows in the sprite sheet
+var columns = 2
+var rows = 2
+var sprite_width = 64
+var sprite_height = 64
 
 signal meteorite_off_screen
 
 # Called when the node enters the scene tree for the first time.
-func _ready() -> void:
+func _ready():
 	pass # Replace with function body.
-
+	# get random sprite texture
+	var random_num = int(randf_range(0,4))
+	
+	# Enable region to show part of the texture
+	sprite_2d.region_enabled = true
+	# Change to the second sprite (1, 0) from the sprite sheet
+	var sprite_x = random_num  # Sets a random number to the Column of the region
+	var sprite_y = 0  # Row
+	# Set the region_rect to show the selected sprite
+	sprite_2d.region_rect = Rect2(sprite_x * sprite_width, sprite_y * sprite_height, sprite_width, sprite_height)
+	
+	spawned_rock = {meteorite_dict.keys()[random_num]:meteorite_dict[meteorite_arr[random_num]]}
+	#print(spawned_rock)
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	position.y += speed * delta
@@ -19,4 +40,5 @@ func _process(delta: float) -> void:
 	if position.y > get_viewport_rect().size.y:
 		meteorite_off_screen.emit()
 		queue_free()
+
 		
